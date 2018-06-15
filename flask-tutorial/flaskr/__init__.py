82e88b0b1c6bd4af6f8e8607cc4224aec269637c
Zane <zane.franklin@gtri.gatech.edu>
2018-06-15 16:36:15 -0400

    tutorial-application setup

---
+++ flask-tutorial/flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev_key",
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # load instance config if it exists (when not testing)
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load passed config (when testing)
+        app.config.from_mapping(test_config)
+
+    # make sure instance dir exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def hello():
+        return "Hello, World!"
+
+    return app
