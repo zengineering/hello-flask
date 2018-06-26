00daf3cf8a362cb109194d52f275f8186f275d3e
Zane <zane.franklin@gtri.gatech.edu>
2018-06-26 11:24:28 -0400

    tutorial: blog blueprint

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -21,14 +21,18 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/")
-    def hello():
-        return "Hello, World!"
-
+#    @app.route("/")
+#    def hello():
+#        return "Hello, World!"
+#
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
